9e156402b122cc64cdaad49268aae6438f4c6cc1
Piotr <piotr.liszka.pl@gmail.com>
2020-11-24 16:21:28 +0100

    Lesson 2.3 - Exercise: Add Baseline Constraint and Button Chain

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/color/ColorMyViewsFragment.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/color/ColorMyViewsFragment.kt
@@ -1,17 +1,15 @@
 package pl.maniak.androidkotlindeveloper.ui.udacity.color
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_color_my_views.*
 import pl.maniak.androidkotlindeveloper.R
 
 class ColorMyViewsFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,5 +17,39 @@ class ColorMyViewsFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_color_my_views, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setListeners()
+    }
+
+    private fun setListeners() {
+        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
+            red_button, green_button, yellow_button)
+        for (item in clickableViews) {
+            item.setOnClickListener { makeColored(it) }
+        }
+    }
+
+    fun makeColored(view: View) {
+        when (view.id) {
+
+            // Boxes using Color class colors for background
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+
+            // Boxes using Android color resources for background
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+
+            // Boxes using custom colors for background
+            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
     }
 }
