9d0c21ed2033ffd35ad0e3990c65cd6c52ba7074
Piotr <piotr.liszka.pl@gmail.com>
2020-11-28 14:03:34 +0100

    Lesson 2.5 - Init project Dessert Pusher

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/home/HomeFragment.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/home/HomeFragment.kt
@@ -5,14 +5,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
 import pl.maniak.androidkotlindeveloper.R
 import pl.maniak.androidkotlindeveloper.databinding.FragmentHomeBinding
+import pl.maniak.androidkotlindeveloper.ui.udacity.dessertpusher.DessertPusherActivity
 import pl.maniak.androidkotlindeveloper.ui.udacity.trivia.TriviaActivity
 
 class HomeFragment : Fragment() {
@@ -32,6 +31,7 @@ class HomeFragment : Fragment() {
             homeApp02.setOnClickListener { findNavController().navigate(R.id.action_nav_home_to_aboutMeFragment) }
             homeApp03.setOnClickListener { findNavController().navigate(R.id.action_nav_home_to_colorMyViewsFragment) }
             homeApp04.setOnClickListener { startActivity(Intent(requireContext(), TriviaActivity::class.java)) }
+            homeApp05.setOnClickListener { startActivity(Intent(requireContext(), DessertPusherActivity::class.java)) }
         }
         return binding.root
     }
---
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/dessertpusher/DessertPusherActivity.kt
@@ -0,0 +1,6 @@
+package pl.maniak.androidkotlindeveloper.ui.udacity.dessertpusher
+
+import androidx.appcompat.app.AppCompatActivity
+import pl.maniak.androidkotlindeveloper.R
+
+class DessertPusherActivity : AppCompatActivity(R.layout.activity_dessert_pusher)
