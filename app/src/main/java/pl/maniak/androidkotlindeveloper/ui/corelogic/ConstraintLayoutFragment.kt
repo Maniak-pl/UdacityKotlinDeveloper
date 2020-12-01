7a90c86847f070872f9b8f9ebaa8ac9dcca59a64
Piotr Liszka <pliszka@corelogic.pl>
2020-12-01 10:05:01 +0100

    Lesson C1.1 Added Baseline, Guideline, Barrier, Circular Positioning

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/corelogic/ConstraintLayoutFragment.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/corelogic/ConstraintLayoutFragment.kt
@@ -1,6 +1,26 @@
 package pl.maniak.androidkotlindeveloper.ui.corelogic
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_constraint_layout.*
 import pl.maniak.androidkotlindeveloper.R
 
-class ConstraintLayoutFragment: Fragment(R.layout.fragment_constraint_layout)
+class ConstraintLayoutFragment : Fragment(R.layout.fragment_constraint_layout) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initListener()
+    }
+
+    private fun initListener() {
+        text16.setOnClickListener {
+            text16.text = "A - long text"
+            text17.text = "B"
+        }
+        text17.setOnClickListener {
+            text16.text = "A"
+            text17.text = "B - long text"
+        }
+    }
+}
