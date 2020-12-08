981f93c080a9eb26cf6f15179a6ebb6e417adf42
Piotr Liszka <pliszka@corelogic.pl>
2020-12-08 11:05:58 +0100

    Lesson 2.5.27. Exercise: LiveData Map Transformation

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/guesstheword/screens/game/GameViewModel.kt
@@ -1,10 +1,11 @@
 package pl.maniak.androidkotlindeveloper.ui.udacity.guesstheword.screens.game
 
 import android.os.CountDownTimer
+import android.text.format.DateUtils
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
-import timber.log.Timber
 
 class GameViewModel : ViewModel() {
 
@@ -25,6 +26,11 @@ class GameViewModel : ViewModel() {
     private val _currentTime = MutableLiveData<Long>()
     val currentTime: LiveData<Long>
         get() = _currentTime
+
+    // The String version of the current time
+    val currentTimeString = Transformations.map(currentTime) { time ->
+        DateUtils.formatElapsedTime(time)
+    }
 
     // The current word
     private val _word = MutableLiveData<String>()
