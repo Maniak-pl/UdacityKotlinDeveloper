c30b762eaeba9d841fd3c85e9eab531a07e258d8
Piotr Liszka <pliszka@corelogic.pl>
2020-12-28 10:33:07 +0100

    Lesson 3.2.15 Exercise: Add Binding Adapters

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -5,10 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import pl.maniak.androidkotlindeveloper.R
 import pl.maniak.androidkotlindeveloper.databinding.ListItemSleepNightBinding
-import pl.maniak.androidkotlindeveloper.ui.udacity.trackmysleepquality.convertDurationToFormatted
-import pl.maniak.androidkotlindeveloper.ui.udacity.trackmysleepquality.convertNumericQualityToString
 import pl.maniak.androidkotlindeveloper.ui.udacity.trackmysleepquality.database.SleepNight
 
 class SleepNightAdapter :
@@ -27,21 +24,8 @@ class SleepNightAdapter :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: SleepNight) {
-            val res = itemView.context.resources
-            binding.sleepLength.text =
-                convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-            binding.qualityImage.setImageResource(
-                when (item.sleepQuality) {
-                    0 -> R.drawable.ic_sleep_0
-                    1 -> R.drawable.ic_sleep_1
-                    2 -> R.drawable.ic_sleep_2
-                    3 -> R.drawable.ic_sleep_3
-                    4 -> R.drawable.ic_sleep_4
-                    5 -> R.drawable.ic_sleep_5
-                    else -> R.drawable.ic_sleep_active
-                }
-            )
+            binding.sleep = item
+            binding.executePendingBindings()
         }
 
         companion object {
