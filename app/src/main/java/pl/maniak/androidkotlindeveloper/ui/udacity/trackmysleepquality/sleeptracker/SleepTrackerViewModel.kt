7edf1be92102cd5b890fcea786c00343559d5fcb
Piotr <piotr.liszka.pl@gmail.com>
2020-12-29 05:55:02 +0100

    Lesson 3.2.23 - Exercise: Navigate on Click

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -68,6 +68,18 @@ class SleepTrackerViewModel(
         _navigateToSleepQuality.value = null
     }
 
+    private val _navigateToSleepDataQuality = MutableLiveData<Long>()
+    val navigateToSleepDataQuality
+        get() = _navigateToSleepDataQuality
+
+    fun onSleepNightClicked(id:Long) {
+        _navigateToSleepDataQuality.value = id
+    }
+
+    fun onSleepDataQualityNavigated() {
+        _navigateToSleepDataQuality.value = null
+    }
+
     private fun initializeTonight() {
         viewModelScope.launch {
             tonight.value = getTonightFromDatabase()
