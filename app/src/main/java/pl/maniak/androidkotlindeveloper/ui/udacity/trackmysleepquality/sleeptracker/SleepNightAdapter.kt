2175b8402327f33cbacb89676f2eabb88da39dfb
Piotr <piotr.liszka.pl@gmail.com>
2020-12-29 05:02:28 +0100

    Lesson 3.2.22 - Exercise: Implement a Click Listener

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -8,12 +8,11 @@ import androidx.recyclerview.widget.RecyclerView
 import pl.maniak.androidkotlindeveloper.databinding.ListItemSleepNightBinding
 import pl.maniak.androidkotlindeveloper.ui.udacity.trackmysleepquality.database.SleepNight
 
-class SleepNightAdapter :
+class SleepNightAdapter(val clickListener: SleepNightListener) :
     ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(SleepNightDiffCallback()) {
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        holder.bind(item)
+        holder.bind(getItem(position), clickListener)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -23,8 +22,9 @@ class SleepNightAdapter :
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(item: SleepNight) {
+        fun bind(item: SleepNight, clickListener: SleepNightListener) {
             binding.sleep = item
+            binding.clickListener = clickListener
             binding.executePendingBindings()
         }
 
@@ -53,4 +53,8 @@ class SleepNightDiffCallback :
     override fun areContentsTheSame(oldItem: SleepNight, newItem: SleepNight): Boolean {
         return oldItem == newItem
     }
+}
+
+class SleepNightListener(val clickListener: (sleepId: Long) -> Unit) {
+    fun onClick(night: SleepNight) = clickListener(night.nightId)
 }
