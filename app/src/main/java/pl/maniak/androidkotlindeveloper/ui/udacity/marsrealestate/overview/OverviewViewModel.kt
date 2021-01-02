078082c0ed13f2c56f7d7ee0091da1395369e009
Piotr <piotr.liszka.pl@gmail.com>
2021-01-02 05:05:21 +0100

    Lesson 3.2.3 Starter Code

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/overview/OverviewViewModel.kt
@@ -3,57 +3,30 @@ package pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.launch
-import pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.network.MarsApi
-import pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.network.MarsApiFilter
-import pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.network.MarsProperty
+
 
 enum class MarsApiStatus { LOADING, ERROR, DONE }
 
 class OverviewViewModel : ViewModel() {
 
-    private val _status = MutableLiveData<MarsApiStatus>()
+    // The internal MutableLiveData String that stores the status of the most recent request
+    private val _response = MutableLiveData<String>()
 
-    val status: LiveData<MarsApiStatus>
-        get() = _status
+    // The external immutable LiveData for the request status String
+    val response: LiveData<String>
+        get() = _response
 
-    private val _properties = MutableLiveData<List<MarsProperty>>()
-
-    val properties: LiveData<List<MarsProperty>>
-        get() = _properties
-
-    private val _navigateToSelectedProperty = MutableLiveData<MarsProperty>()
-
-    val navigateToSelectedProperty: LiveData<MarsProperty>
-        get() = _navigateToSelectedProperty
-
+    /**
+     * Call getMarsRealEstateProperties() on init so we can display status immediately.
+     */
     init {
-        getMarsRealEstateProperties(MarsApiFilter.SHOW_ALL)
+        getMarsRealEstateProperties()
     }
 
-     private fun getMarsRealEstateProperties(filter: MarsApiFilter) {
-        viewModelScope.launch {
-            _status.value = MarsApiStatus.LOADING
-            try {
-                _properties.value = MarsApi.retrofitService.getProperties(filter.value)
-                _status.value = MarsApiStatus.DONE
-            } catch (e: Exception) {
-                _status.value = MarsApiStatus.ERROR
-                _properties.value = ArrayList()
-            }
-        }
-    }
-
-    fun displayPropertyDetails(marsProperty: MarsProperty) {
-        _navigateToSelectedProperty.value = marsProperty
-    }
-
-    fun displayPropertyDetailsComplete() {
-        _navigateToSelectedProperty.value = null
-    }
-
-    fun updateFilter(filter: MarsApiFilter) {
-        getMarsRealEstateProperties(filter)
+    /**
+     * Sets the value of the status LiveData to the Mars API status.
+     */
+    private fun getMarsRealEstateProperties() {
+        _response.value = "Set the Mars API Response here!"
     }
 }
