77c0c19c2c967b400924f1da9cd8e88a087eb2dd
Piotr <piotr.liszka.pl@gmail.com>
2021-01-04 04:43:17 +0100

    Update: Migrate from Kotlin synthetics to Jetpack view binding

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/corelogic/ConstraintLayoutFragment.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/corelogic/ConstraintLayoutFragment.kt
@@ -1,26 +1,35 @@
 package pl.maniak.androidkotlindeveloper.ui.corelogic
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_constraint_layout.*
 import pl.maniak.androidkotlindeveloper.R
+import pl.maniak.androidkotlindeveloper.databinding.FragmentConstraintLayoutBinding
 
-class ConstraintLayoutFragment : Fragment(R.layout.fragment_constraint_layout) {
+class ConstraintLayoutFragment : Fragment() {
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initListener()
-    }
+    private lateinit var binding: FragmentConstraintLayoutBinding
 
-    private fun initListener() {
-        text16.setOnClickListener {
-            text16.text = "A - long text"
-            text17.text = "B"
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_constraint_layout, container, false)
+        binding.apply {
+            text16.setOnClickListener {
+                text16.text = "A - long text"
+                text17.text = "B"
+            }
+            text17.setOnClickListener {
+                text16.text = "A"
+                text17.text = "B - long text"
+            }
         }
-        text17.setOnClickListener {
-            text16.text = "A"
-            text17.text = "B - long text"
-        }
+        return binding.root
     }
 }
--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/color/ColorMyViewsFragment.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/color/ColorMyViewsFragment.kt
@@ -2,21 +2,26 @@ package pl.maniak.androidkotlindeveloper.ui.udacity.color
 
 import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_color_my_views.*
 import pl.maniak.androidkotlindeveloper.R
+import pl.maniak.androidkotlindeveloper.databinding.FragmentColorMyViewsBinding
 
 class ColorMyViewsFragment : Fragment() {
+
+    private lateinit var binding: FragmentColorMyViewsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_color_my_views, container, false)
+        binding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_color_my_views, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -25,10 +30,14 @@ class ColorMyViewsFragment : Fragment() {
     }
 
     private fun setListeners() {
-        val clickableViews: List<View> = listOf(box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
-            red_button, green_button, yellow_button)
-        for (item in clickableViews) {
-            item.setOnClickListener { makeColored(it) }
+        binding.apply {
+            val clickableViews: List<View> = listOf(
+                box_one_text, box_two_text, box_three_text, box_four_text, box_five_text,
+                red_button, green_button, yellow_button
+            )
+            for (item in clickableViews) {
+                item.setOnClickListener { makeColored(it) }
+            }
         }
     }
 
@@ -45,9 +54,9 @@ class ColorMyViewsFragment : Fragment() {
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
             // Boxes using custom colors for background
-            R.id.red_button -> box_three_text.setBackgroundResource(R.color.my_red)
-            R.id.yellow_button -> box_four_text.setBackgroundResource(R.color.my_yellow)
-            R.id.green_button -> box_five_text.setBackgroundResource(R.color.my_green)
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
