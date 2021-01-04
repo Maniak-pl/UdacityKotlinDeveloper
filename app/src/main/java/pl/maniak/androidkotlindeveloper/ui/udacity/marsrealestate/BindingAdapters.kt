d15c5fea611f4359c22e576c8caee8ad8b6f2842
Piotr Liszka <pliszka@corelogic.pl>
2021-01-04 09:43:40 +0100

    Lesson 3.3.13 Exercise: Display Images in a Grid

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/BindingAdapters.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/BindingAdapters.kt
@@ -3,9 +3,18 @@ package pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import pl.maniak.androidkotlindeveloper.R
+import pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.network.MarsProperty
+import pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.overview.PhotoGridAdapter
+
+@BindingAdapter("listData")
+fun bindRecyclerView(recyclerView: RecyclerView, data: List<MarsProperty>?) {
+    val adapter = recyclerView.adapter as PhotoGridAdapter
+    adapter.submitList(data)
+}
 
 @BindingAdapter("imageUrl")
 fun bindImage(imgView: ImageView, imgUrl: String?) {
--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/overview/OverviewFragment.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/overview/OverviewFragment.kt
@@ -5,7 +5,7 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import pl.maniak.androidkotlindeveloper.R
-import pl.maniak.androidkotlindeveloper.databinding.GridViewItemBinding
+import pl.maniak.androidkotlindeveloper.databinding.FragmentOverviewBinding
 
 
 class OverviewFragment : Fragment() {
@@ -18,13 +18,15 @@ class OverviewFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val binding = GridViewItemBinding.inflate(inflater)
+        val binding = FragmentOverviewBinding.inflate(inflater)
 
         // Allows Data Binding to Observe LiveData with the lifecycle of this Fragment
         binding.lifecycleOwner = this
 
         // Giving the binding access to the OverviewViewModel
         binding.viewModel = viewModel
+
+        binding.photosGrid.adapter = PhotoGridAdapter()
 
         setHasOptionsMenu(true)
         return binding.root
