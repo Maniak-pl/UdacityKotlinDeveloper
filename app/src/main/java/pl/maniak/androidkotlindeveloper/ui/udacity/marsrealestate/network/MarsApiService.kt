18660d5e9a927d3b6875112a1d2d8e48f7e7e65e
Piotr <piotr.liszka.pl@gmail.com>
2021-01-04 05:13:42 +0100

    Lesson 3.3.10 Exercise: Coroutines and Deferred

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/network/MarsApiService.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/marsrealestate/network/MarsApiService.kt
@@ -2,7 +2,6 @@ package pl.maniak.androidkotlindeveloper.ui.udacity.marsrealestate.network
 
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import retrofit2.Call
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
@@ -32,7 +31,7 @@ interface MarsApiService {
      * HTTP method
      */
     @GET("realestate")
-    fun getProperties(): Call<List<MarsProperty>>
+    suspend fun getProperties(): List<MarsProperty>
 }
 
 /**
