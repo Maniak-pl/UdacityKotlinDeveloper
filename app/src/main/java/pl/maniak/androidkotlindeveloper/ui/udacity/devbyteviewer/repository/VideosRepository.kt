23ea14c3cc08f6a69e89e32b091b9ce7cc7e4841
Piotr <piotr.liszka.pl@gmail.com>
2021-01-06 14:04:09 +0100

    Lesson 3.4.15 Exercise: Build a Repository

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/devbyteviewer/repository/VideosRepository.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/devbyteviewer/repository/VideosRepository.kt
@@ -1,1 +1,26 @@
 package pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.repository
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Transformations
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.database.VideosDatabase
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.database.asDomainModel
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.domain.Video
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.network.Network
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.network.asDatabaseModel
+
+class VideosRepository(private val database: VideosDatabase) {
+
+    val videos: LiveData<List<Video>> =
+        Transformations.map(database.videoDao.getVideos()) {
+            it.asDomainModel()
+        }
+
+    suspend fun refreshVideos() {
+        withContext(Dispatchers.IO) {
+            val playlist = Network.devbytes.getPlaylist().await()
+            database.videoDao.insertAll(*playlist.asDatabaseModel())
+        }
+    }
+}
