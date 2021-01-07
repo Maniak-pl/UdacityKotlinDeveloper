4c9a50668484419bc9c1a4af1ab9a2176ec3ae33
Piotr <piotr.liszka.pl@gmail.com>
2021-01-07 04:50:47 +0100

    Lesson 3.4.19. Exercise: Create a Worker

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/devbyteviewer/work/RefreshDataWork.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/devbyteviewer/work/RefreshDataWork.kt
@@ -1,1 +1,26 @@
 package pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.work
+
+import android.content.Context
+import androidx.work.CoroutineWorker
+import androidx.work.WorkerParameters
+import com.bumptech.glide.load.HttpException
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.database.getDatabase
+import pl.maniak.androidkotlindeveloper.ui.udacity.devbyteviewer.repository.VideosRepository
+
+class RefreshDataWorker(appContext: Context, params: WorkerParameters) :
+    CoroutineWorker(appContext, params) {
+
+    /**
+     * A coroutine-friendly method to do your work.
+     */
+    override suspend fun doWork(): Result {
+        val database = getDatabase(applicationContext)
+        val repository = VideosRepository(database)
+        return try {
+            repository.refreshVideos()
+            Result.success()
+        } catch (e: HttpException) {
+            Result.retry()
+        }
+    }
+}
