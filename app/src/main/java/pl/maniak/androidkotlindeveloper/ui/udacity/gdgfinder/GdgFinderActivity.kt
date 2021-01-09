f0864abec23133bd41b5243551647c401fe49827
Piotr <piotr.liszka.pl@gmail.com>
2021-01-09 06:27:27 +0100

    Lesson 3.5.28 Exercise: Add Dark Mode Support

--- app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/gdgfinder/GdgFinderActivity.kt
+++ app/src/main/java/pl/maniak/androidkotlindeveloper/ui/udacity/gdgfinder/GdgFinderActivity.kt
@@ -3,6 +3,7 @@ package pl.maniak.androidkotlindeveloper.ui.udacity.gdgfinder
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
@@ -22,6 +23,8 @@ class GdgFinderActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_gdg_finder)
 
         setupNavigation()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
     }
 
     /**
