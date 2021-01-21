0f44613b9f20b7c23d8880a415912c12b542bf9f
Piotr <piotr.liszka.pl@gmail.com>
2021-01-21 06:23:09 +0100

    Lesson 4.4.8 Exercise: Override onSizeChanged()

--- app/src/main/java/pl/maniak/developer/ui/udacity/minipaint/MyCanvasView.kt
+++ app/src/main/java/pl/maniak/developer/ui/udacity/minipaint/MyCanvasView.kt
@@ -1,7 +1,29 @@
 package pl.maniak.developer.ui.udacity.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import pl.maniak.developer.R
 
 class MyCanvasView(context: Context) : View(context) {
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        //Looking at onSizeChanged(), a new bitmap and canvas are created every time the function executes.
+        // You need a new bitmap, because the size has changed.
+        // However, this is a memory leak, leaving the old bitmaps around.
+        // To fix this, recycle extraBitmap before creating the next one.
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+    }
 }
