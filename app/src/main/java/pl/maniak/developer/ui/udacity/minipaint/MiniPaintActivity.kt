928ec5af2dbd4aaf5a966a6fbee768385189488c
Piotr <piotr.liszka.pl@gmail.com>
2021-01-21 06:10:50 +0100

    Lesson 4.4.7 Exercise: Set MyCanvasView as the Content View

--- app/src/main/java/pl/maniak/developer/ui/udacity/minipaint/MiniPaintActivity.kt
+++ app/src/main/java/pl/maniak/developer/ui/udacity/minipaint/MiniPaintActivity.kt
@@ -2,11 +2,15 @@ package pl.maniak.developer.ui.udacity.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 import pl.maniak.developer.R
 
 class MiniPaintActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mini_paint)
+        val myCanvasView = MyCanvasView(this)
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+        setContentView(myCanvasView)
     }
 }
