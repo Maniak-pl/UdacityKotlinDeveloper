228b077b13e398511c3d118a908fc4569c536b80
Piotr <piotr.liszka.pl@gmail.com>
2021-02-12 04:39:48 +0100

    Lesson 5.1.6 Adding Markers

--- app/src/main/java/pl/maniak/developer/ui/udacity/wander/WanderActivity.kt
+++ app/src/main/java/pl/maniak/developer/ui/udacity/wander/WanderActivity.kt
@@ -29,10 +29,13 @@ class WanderActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val latitude = 50.06661269784586
+        val longitude =19.90595239683685
+        val work = LatLng(latitude, longitude)
+
+        val zoomLevel = 15f
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(work, zoomLevel))
+        map.addMarker(MarkerOptions().position(work))
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
