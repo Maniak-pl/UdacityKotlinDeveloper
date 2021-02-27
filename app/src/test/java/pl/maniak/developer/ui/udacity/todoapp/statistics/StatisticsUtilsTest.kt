ac781b4ba44aeb7ce38481d7217a8717bf27b36e
Piotr <piotr.liszka.pl@gmail.com>
2021-02-27 05:43:02 +0100

    Lesson 5.3.11 Make Your Tests Readable

--- app/src/test/java/pl/maniak/developer/ui/udacity/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/pl/maniak/developer/ui/udacity/todoapp/statistics/StatisticsUtilsTest.kt
@@ -1,5 +1,6 @@
 package pl.maniak.developer.ui.udacity.todoapp.statistics
 
+import org.hamcrest.CoreMatchers.`is`
 import org.junit.Assert.*
 import org.junit.Test
 import pl.maniak.developer.ui.udacity.todoapp.data.Task
@@ -11,16 +12,17 @@ class StatisticsUtilsTest {
     @Test
     fun getActiveAndCompletedStats_noCompleted_returnsHundredZero() {
 
-        // Create an active tasks (the false makes this active)
+        // GIVEN a list of tasks with a single, active, task
         val tasks = listOf<Task>(
             Task("title", "desc", isCompleted = false)
         )
-        // Call our function
+
+        // WHEN you call getActiveAndCompleteStats
         val result = getActiveAndCompletedStats(tasks)
 
-        // Check the result
-        assertEquals(result.completedTasksPercent, 0f)
-        assertEquals(result.activeTasksPercent, 100f)
+        // THEN there are 0% completed tasks and 100% active tasks
+        assertThat(result.completedTasksPercent, `is`(0f))
+        assertThat(result.activeTasksPercent, `is`(100f))
     }
 
     // If there's 2 completed tasks and 3 active tasks,
