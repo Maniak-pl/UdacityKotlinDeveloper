0a8da1a090a4d7ff5e2392eaf0f37794793e05b5
Piotr <piotr.liszka.pl@gmail.com>
2021-02-28 07:14:21 +0100

    Lesson 5.3.16 LiveData Testing Basics

--- app/src/test/java/pl/maniak/developer/ui/udacity/todoapp/tasks/TasksViewModelTest.kt
+++ app/src/test/java/pl/maniak/developer/ui/udacity/todoapp/tasks/TasksViewModelTest.kt
@@ -1,12 +1,21 @@
 package pl.maniak.developer.ui.udacity.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.not
+import org.hamcrest.CoreMatchers.nullValue
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import pl.maniak.developer.ui.udacity.todoapp.getOrAwaitValue
 
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -18,6 +27,7 @@ class TasksViewModelTest {
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
-        // TODO test LiveData
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), (not(nullValue())))
     }
 }
